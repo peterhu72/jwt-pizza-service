ab48f0cb10bdb7da6658044cc799e91b2b67fd4b
peterhu72 <peterhu4000@gmail.com>
2025-02-03 21:11:51 -0700

    eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  pluginJs.configs.recommended
+];
